d5a6022cb3c9a1326b743ac9ebdf884f6437ee2f
CaldatoLuca <lucacaldato15@gmail.com>
2024-06-24 14:44:15 +0200

    feat: creazione context di autenticazione

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,22 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container mx-auto lining-nums">
-        <Routes>
-          <Route path="/" element={<BaseLayout />}>
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="/posts" element={<BaseLayout />}>
-            <Route path="details/:slug" element={<Details />} />
-            <Route path=":tag" element={<PostsByTag />} />
-          </Route>
-          <Route path="/access" element={<AccessLayout />}>
-            <Route path="login" element={<Login />} />
-            {/* <Route path="register" element={<PostsByTag />} /> */}
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="container mx-auto lining-nums">
+      <Routes>
+        <Route path="/" element={<BaseLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/posts" element={<BaseLayout />}>
+          <Route path="details/:slug" element={<Details />} />
+          <Route path=":tag" element={<PostsByTag />} />
+        </Route>
+        <Route path="/access" element={<AccessLayout />}>
+          <Route path="login" element={<Login />} />
+          {/* <Route path="register" element={<PostsByTag />} /> */}
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
